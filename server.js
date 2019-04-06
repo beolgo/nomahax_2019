2f4ba5773cd8c786cea8a80aa75fb55a981ff7ea
jeph88 <jeffreyolson224@gmail.com>
2019-04-05 20:27:33 -0700

    Basic Framework

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
+
+const mysql = require('mysql');
+const conn = mysql.createConnection({
+	host : "localhost",
+	user : "root",
+	password : "cheese88",
+	database : "NomaHacks"
+});
+
+conn.connect();
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route, send string back to client
+// create a GET route
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
